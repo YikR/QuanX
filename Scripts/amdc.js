94785e92ce0d52a4e8af8f58fb34711f0dbeb193
Cube <49463634+RuCu6@users.noreply.github.com>
2023-03-31 15:16:31 +0800

    Update amdc.js

--- Scripts/amdc.js
+++ Scripts/amdc.js
@@ -1,15 +1,17 @@
-// 2023-03-31 15:08
+// 2023-03-31 15:15
 
 const url = $request.url;
 const header = $request.headers;
 let ua = header["User-Agent"] || header["user-agent"];
 
-if (ua.includes("AMap") || ua.includes("Cainiao")) {
-  if (typeof $task !== "undefined") {
-    $done({ status: "HTTP/1.1 404 Not Found" });
+if (url.includes("/amdc/mobileDispatch")) {
+  if (ua.includes("AMap") || ua.includes("Cainiao")) {
+    if (typeof $task !== "undefined") {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done();
+    }
   } else {
-    $done();
+    $done({});
   }
-} else {
-  $done({});
 }
